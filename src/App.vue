7b5ca8194220fa6ab855b24ecf6809c0db96da89
SakinalaRaj <143786644+SakinalaRaj@users.noreply.github.com>
2023-10-11 18:00:48 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h2>Google font icons</h2>
+  <div>
+    <button>
+      <span class="material-symbols-outlined">search</span>
+    </button>
+    <button>
+      <span class="material-symbols-outlined">home</span>
+    </button>
+    <button>
+      <span class="material-symbols-outlined">settings</span>
+    </button>
+    <button>
+      <span class="material-symbols-outlined">favorite</span>
+    </button>
+  </div>
+  <h2>Material design icons</h2>
+  <div>
+    <button>
+      <span class="material-symbols-outlined">search</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+// import SvgIcon from "@jamescoyle/vue-icon";
+// import { mdiCurrencyInr } from "@mdi/js";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
